b0e1d9ce6d36cac34f6375dde258f99442263f4a
swastikaa0 <swastikadhungana2004@gmail.com>
2025-06-25 13:59:05 +0545

    Add Workout

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from './Login'
 import Signup from './Signup'
+import AddWorkout from './AddWorkout'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
       <LoginPage />
       <Signup />
+      <AddWorkout/>
       
     </>
   )
